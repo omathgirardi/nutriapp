06688444fb2cf77de25540388468c6ba7db166c6
[lasy] <git@lasy.ai>
2025-07-04 01:31:56 -0300

    [lasy] Limpando arquivos duplicados do projeto - deleted 37 file(s)

--- src/app/(dashboard)/calculator/edit/layout.jsx
+++
@@ -1,3 +0,0 @@
-export default function EditDietLayout({ children }) {
-  return <>{children}</>;
-}
--- src/app/(dashboard)/profile/buy-credits/layout.jsx
+++
@@ -1,3 +0,0 @@
-export default function BuyCreditsLayout({ children }) {
-  return <>{children}</>;
-}
